dc63028324eadfe2e8a17638b16ab473355882b4
ebeshero <ebbondar@gmail.com>
2022-03-03 02:55:58 -0500

    scraped avatar scripts via Beautiful Soup

--- python-scraping/scriptParser.py
+++ python-scraping/scriptParser.py
@@ -6,54 +6,30 @@
 #
 import os
 import bs4
-# ebb: be sure to do pip install lxml (on mac os)
-import lxml
-from lxml import html
-from lxml import etree
-import io
-from io import StringIO, BytesIO
+from bs4 import BeautifulSoup
+import requests
 import glob
-
-
-
-# archive_url = 'https://blueslyrics.tripod.com/bluessongs1.htm'
 path = 'avatarTranscripts'
 scriptDir = os.listdir(path)
-print(scriptDir)
+# print(scriptDir)
 
 for filenames in scriptDir:
     try:
          with open(os.path.join(path, filenames), encoding="utf8", errors='ignore') as file:
-            filerename = file.name + "xml"
-            print(filerename)
+            filerename = file.name.split(':')[1] + ".xml"
+            # print(filerename)
             filename = 'avTrans/' + filerename
             # print(filename)
             content = file.read()
             file.close()
-            parser = etree.HTMLParser()
-            tree = etree.parse(StringIO(content), parser)
-            rootParse = etree.tostring(tree.getroot(), pretty_print=True, method="html")
-            # print(rootParse)
-            stringRoot = str(rootParse, 'UTF-8')
-            parsedRoot = etree.parse(StringIO(stringRoot), parser)
-            metatitle = parsedRoot.xpath('//title/text()')
-            print(metatitle)
-            songtitle = parsedRoot.xpath('//table[@border="0"]//text()')
-            # print(songtitle)
-            converted_songtitle = []
-            for wordstuff in songtitle:
-                converted_songtitle.append(wordstuff.strip())
-                # print(converted_songtitle)
-            songtitle2 = [x.replace('\n         ', ' ') for x in converted_songtitle]
-            # print(songtitle2)
-            songlyrics = parsedRoot.xpath('//table[@border="0"]/following::text()[not(parent::b)][not(parent::font[@size="-1"])]')
-                # print(songlyrics)
+            soup = BeautifulSoup(content, 'html.parser')
+            script = soup.find('table', { "class" : "wikitable"})
+            # print(script)
 
-                newfile = open(filename, "w")
-                contents0 = '\n'.join(metatitle)
-                contents1 = '\n'.join(songtitle2)
-                contents2 = '\n'.join(songlyrics)
-                newfile.write(contents0 + contents1 + contents2)
-                newfile.close()
+            newfile = open(filename, "w", encoding = 'utf-8')
+            # scriptContents = '\n'.join(script)
+            # print(scriptContents)
+            newfile.write(str(script))
+            newfile.close()
     except:
         continue
